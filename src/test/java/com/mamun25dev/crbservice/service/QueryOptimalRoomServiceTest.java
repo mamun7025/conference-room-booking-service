e3233a509ebcf001d146623d2032277f3f28d843
mamun7025 <mamun7025@gmail.com>
2024-03-27 01:33:18 +0400

    overlap time unit test case

--- src/test/java/com/mamun25dev/crbservice/service/QueryOptimalRoomServiceTest.java
+++ src/test/java/com/mamun25dev/crbservice/service/QueryOptimalRoomServiceTest.java
@@ -2,8 +2,10 @@ package com.mamun25dev.crbservice.service;
 
 import com.mamun25dev.crbservice.TestUtils;
 import com.mamun25dev.crbservice.dto.command.QueryOptimalRoomCommand;
+import com.mamun25dev.crbservice.exception.BusinessException;
 import com.mamun25dev.crbservice.repository.ConferenceRoomRepository;
 import com.mamun25dev.crbservice.service.adapter.QueryOptimalRoomServiceImpl;
+import com.mamun25dev.crbservice.service.adapter.QuerySlotServiceImpl;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,18 +23,18 @@ public class QueryOptimalRoomServiceTest {
     private QueryOptimalRoomServiceImpl queryOptimalRoomService;
 
     @Spy
-    private QuerySlotService querySlotService;
+    private QuerySlotServiceImpl querySlotService;
 
     @Mock
     private ConferenceRoomRepository conferenceRoomRepository;
 
     @Test
-    @DisplayName("test optimal room search")
+    @DisplayName("optimal room search test")
     public void test_optimal_room_search(){
         // given
         final var searchCommand = QueryOptimalRoomCommand.builder()
                 .noOfParticipant(3)
-                .meetingStartTime(LocalTime.of(10, 00))
+                .meetingStartTime(LocalTime.of(10, 0))
                 .meetingEndTime(LocalTime.of(10, 30))
                 .build();
 
@@ -46,6 +48,30 @@ public class QueryOptimalRoomServiceTest {
         Assertions.assertTrue(response.isPresent());
         Assertions.assertEquals("501", response.get().getCode());
         Assertions.assertEquals("Amaze", response.get().getName());
+    }
+
+    @Test
+    @DisplayName("test with maintenance overlapped time")
+    public void test_with_maintenance_overlapped_time(){
+        // given
+        final var searchCommand = QueryOptimalRoomCommand.builder()
+                .noOfParticipant(3)
+                .meetingStartTime(LocalTime.of(13, 0))
+                .meetingEndTime(LocalTime.of(13, 30))
+                .build();
+
+        final var conferenceRoomList = TestUtils.getRoomsWithSlots_overlapped();
+        Mockito.when(conferenceRoomRepository.findAllByCapacityGreaterThanEqual(3)).thenReturn(conferenceRoomList);
+
+
+        // when
+        final var exception = Assertions.assertThrows(
+                BusinessException.class,
+                () -> queryOptimalRoomService.query(searchCommand));
+
+
+        // then
+        Assertions.assertEquals("CR-0004", exception.getErrorCode());
     }
 
 }
